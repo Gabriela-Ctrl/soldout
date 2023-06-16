9ac1e3037c457096e8113052daab982a620f3630
EduENL <eduardonuneslima099@gmail.com>
2023-06-16 16:16:22 -0300

    attfinal
    possivel atualização final

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ function menuShow() {
         menuMobile.classList.add('open');
     }
 }   
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-160px";
+  }
+  prevScrollpos = currentScrollPos;
+}
